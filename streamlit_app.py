33eee88264ca59c5da5c72051007ddb56e15ea37
Nick Gasser <ngasser@berkeley.edu>
2025-03-16 10:41:50 -0500

    app development 20250315

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,30 @@
 import streamlit as st
 
-st.title('Recipe Generator')
+# #title
+# st.title('Recipe Generator')
+st.logo("logo.png")
 
-st.write('Hello world!')
+#set configurations
+st.set_page_config(
+    layout="wide",
+    initial_sidebar_state="expanded",
+    page_icon=":sushi:"
+)
+
+pages = [
+    st.Page(
+        "pages/home_page.py",
+        title="Home",
+        icon=":material/home:"),
+    st.Page(
+        "pages/image_upload.py",
+        title="Upload Images",
+        icon=":material/add_photo_alternate:"),
+    st.Page(
+        "pages/recipe_gen.py",
+        title="Generate Recipe",
+        icon=":material/receipt_long:"),
+]
+
+pg = st.navigation(pages)
+pg.run()
